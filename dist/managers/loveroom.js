1e19316c41bdcecf9ab6d589d0d9215fdabc0ff3
Rasl <ruslan170573@mail.ru>
2022-03-02 11:52:04 +0400

    master

--- dist/managers/loveroom.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const LoveroomManager_1 = __importDefault(require("../structures/managers/LoveroomManager"));
-const loveroomManager = new LoveroomManager_1.default();
-exports.default = loveroomManager;
-//# sourceMappingURL=loveroom.js.map
